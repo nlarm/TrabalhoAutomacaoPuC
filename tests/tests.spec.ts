9241ae457dd4a88824ec5bab60630d81833c9e5b
nlarm <53651415+nlarm@users.noreply.github.com>
2024-04-20 22:37:24 -0300

    Adding more tests (#2)
    * Adding Creating a new account test and fixing expect result
    
    Co-authored-by: Nefi Armelim <Nefi Armelim@DESKTOP-6H7HU8D>

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -23,9 +23,37 @@ test('Creating an existing account', async ({page}) => {
 
     const accountPage = new AccountPage(page);
 
-    await accountPage.navigate("https://magento.softwaretestingboard.com/customer/account/create/")
-    await accountPage.fillOutForm("Galvao","Bueno","galvaoBueno@globo.com","SBTEhMelhorqueAGloboForEv3er","SBTEhMelhorqueAGloboForEv3er")
+    await accountPage.navigateCreatAccount()
+    await accountPage.fillOutForm(accountPage.userFirstName,accountPage.userLastName,accountPage.userEmail, accountPage.passwordFillout,accountPage.passwordFillout)
     await accountPage.clickSubmit()
-    await expect(accountPage.checkErrorMessage()).toBeTruthy();
     
+    const result = await accountPage.checkErrorMessage(page);
+    expect(result).toBeTruthy();
+});
+
+test('Creating a new account', async ({page}) => {
+
+    const accountPage = new AccountPage(page);
+    const nomeDate = `Galvao_${Date.now()}_`;
+
+    await accountPage.navigateCreatAccount()
+    await accountPage.fillOutForm(nomeDate,accountPage.userLastName,nomeDate+accountPage.userEmail,accountPage.passwordFillout,accountPage.passwordFillout)
+    await accountPage.clickSubmit()
+
+    const result = await accountPage.checkSuccessMessage(page);
+    expect(result).toBeTruthy();
+});
+
+test('Check password is the same', async ({page}) => {
+
+    const accountPage = new AccountPage(page);
+    const nomeDate = `Galvao_${Date.now()}_`;
+    const wrongPassword = "MentiraAGloboEhMelhor"
+
+    await accountPage.navigateCreatAccount()
+    await accountPage.fillOutForm(nomeDate,accountPage.userLastName,nomeDate+accountPage.userEmail,accountPage.passwordFillout,wrongPassword)
+    await accountPage.clickSubmit()
+
+    const result = await accountPage.checkPasswordError(page);
+    expect(result).toBeTruthy();
 });
